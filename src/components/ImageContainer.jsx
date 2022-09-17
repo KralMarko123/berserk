f4b7e2f75d418207c68a4f56b9066fb6f6f0cbad
KralMarko123 <kralmarko123@gmail.com>
2022-09-17 23:57:54 +0200

    major reset

--- src/components/ImageContainer.jsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const ImageContainer = ({ image }) => {
-  return <img src={image} className="image" />;
-};
-
-export default ImageContainer;
---
+++ src/pages/Home.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import "../styles/general/page.css";
+
+const Home = () => {
+	return <div className="home page">Home</div>;
+};
+
+export default Home;
